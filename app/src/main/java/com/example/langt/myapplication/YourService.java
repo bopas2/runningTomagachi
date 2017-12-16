b8f51b08eb87c20709f440b9d9c938f69813f142
kamottowa <alexbowman66@gmail.com>
2017-12-16 15:17:21 -0500

    Update

--- app/src/main/java/com/example/langt/myapplication/YourService.java
+++ app/src/main/java/com/example/langt/myapplication/YourService.java
@@ -8,18 +8,22 @@ import android.support.annotation.Nullable;
 /**
  * Created by Lpars on 12/16/2017.
  */
-
-public class YourService extends Service
-{
+//Pedometer
+public class YourService extends Service implements StepListener{
+    int stepCount = 0;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // do your jobs here
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void step() {
+        stepCount++;
     }
 }
