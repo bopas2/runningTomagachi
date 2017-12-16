81b945191ccf0264cecf44a9881eae3d762622ff
kamottowa <alexbowman66@gmail.com>
2017-12-16 14:38:58 -0500

    Pedometer

--- app/src/main/java/com/example/langt/myapplication/Accelerameter.java
+++ app/src/main/java/com/example/langt/myapplication/Accelerameter.java
@@ -1,40 +1,29 @@
 package com.example.langt.myapplication;
-
 import android.content.Context;
 import android.hardware.*;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import java.lang.*;
 /**
  * Created by Alex on 12/16/2017.
  */
-
 public class Accelerameter implements SensorEventListener{
-    SensorManager sensorManager;
-    float x,y,z;
-    public void Accelerameter(Context context){
+    private SensorManager sensorManager;
+    StepListener listener;
+    public Accelerameter(Context context, StepListener listener){
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        this.listener = listener;
     }
-
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        x = sensorEvent.values[0];
-        y = sensorEvent.values[1];
-        z = sensorEvent.values[2];
+        listener.step();
     }
-
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
-
     public void SetUp(){
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER), SensorManager.SENSOR_DELAY_NORMAL);
     }
     public void Destroy(){
         sensorManager.unregisterListener(this);
-    }
-    public float[] Retrieve(){
-        return new float[] {x,y,z};
     }
 }
---
+++ app/src/main/java/com/example/langt/myapplication/StepListener.java
@@ -0,0 +1,9 @@
+package com.example.langt.myapplication;
+
+/**
+ * Created by Alex on 12/16/2017.
+ */
+
+public interface StepListener{
+    void step();
+}
